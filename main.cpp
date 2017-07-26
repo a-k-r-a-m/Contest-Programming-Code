9c90438e349a4317465c1483d7d656b176133b5c
Akram Khan <akrammiru@gmail.com>
2017-07-27 01:08:57 +0600

    Circle Elimination - CSAcademy

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void solution(){
-    int n;
-    cin>>n;
-    if(n==0) return;
-    cout<<n/2<<endl;
-    solution();
-}
 int main() {
-    solution();
-    return 0;
+    int n, a;
+    cin>>n;
+    vector<pair<int, int>> m;
+    pair<int, int> p;
+    for(int i=1; i<=n; i++){
+        cin>>a;
+        p.first = a;
+        p.second = i;
+        m.push_back(p);
+    }
+    vector<pair<int, int>> ::iterator it;
+    sort(m.begin(), m.end());
+    it=m.begin();
+    int temp=it->second;
+    long long sum=0;
+    it++;
+    for(; it!=m.end(); it++){
+        sum+=min(fabs(temp - it->second), n - fabs(temp - it->second));
+        temp=it->second;
+    }
+    cout<<sum<<endl;
 }
